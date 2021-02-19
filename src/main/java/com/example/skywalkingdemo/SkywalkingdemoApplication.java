e4570b706f78626ffb1da8d42691177250147354
516544181@qq.com <bzg@123456>
2021-02-19 10:46:18 +0800

    Initial commit

---
+++ src/main/java/com/example/skywalkingdemo/SkywalkingdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.skywalkingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkywalkingdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkywalkingdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skywalkingdemo/SkywalkingdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skywalkingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkywalkingdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
